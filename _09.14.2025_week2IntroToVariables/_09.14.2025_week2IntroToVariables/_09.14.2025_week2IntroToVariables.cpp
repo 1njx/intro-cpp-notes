c9d219a98cfec06b7563db0a555e0f1f9b54c3ab
1njx <brokuberry145@gmail.com>
2025-11-24 02:03:33 -0600

    Add files via upload

---
+++ _09.14.2025_week2IntroToVariables/_09.14.2025_week2IntroToVariables/_09.14.2025_week2IntroToVariables.cpp
@@ -0,0 +1,37 @@
+// _09.14.2025_week2IntroToVariables.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	// int - whole number (integer)
+	int number1 = 5;
+	cout << number1 << "\n";
+
+	int total = number1 * 100;
+	cout << total << "\n";
+
+	// float - decimal number that goes up to 7 decimals
+	float number2 = 3.14;
+
+	// double - decimal number that goes up to 15 decimal places
+	double number3 = 3.14;
+
+	// char - single keyboard character (ascii table)
+	char letter = 'B';
+	
+	// string - multiple characters
+	string word2 = "pie";
+	string word1 = "i love";
+	string sentence = word1 + " " + word2;
+	cout << sentence + "\n";
+
+	// bool - true or false (boolean)
+	bool check = true;
+	cout << check;
+
+	cout << "\nHello World!\n";
+}//end main
